d5326e3b500d5c84e393bf815bba26827626aab3
Matt Glenn <highopenended@gmail.com>
2026-04-21 10:13:14 -0400

    Starting implementation of new game lore

---
+++ src/gameLore/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Game Lore
+ *
+ * Central entry point for lore content (factions, locations, events, etc.).
+ * Keep lore data modular and export it from here for use in-game.
+ */
+
+export {};
